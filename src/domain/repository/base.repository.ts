26ceb026d42c49b66633244185ffb99770b685b2
Sebastian Ospina <jospina@ubits.co>
2024-02-21 17:08:11 -0500

    Make update method partial

--- src/domain/repository/base.repository.ts
+++ src/domain/repository/base.repository.ts
@@ -4,7 +4,7 @@ export interface BaseRepository<T, ID> {
   getById(id: ID): Promise<T | undefined>;
   search(options?: IUbitsFilter): Promise<T[]>;
   create(entity: T): Promise<T>;
-  update(id: ID, updatedEntity: T): Promise<T | undefined>;
+  update(id: ID, updatedEntity: Partial<T>): Promise<T | undefined>;
   deleteById(id: ID): Promise<void>;
 
   inputFormat(data: any, args?: any): T;
